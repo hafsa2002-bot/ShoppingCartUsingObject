25c0af59de1cf619961b13a61553e9e28a23f667
Barhoud Hafsa <barhoudhafsa@gmail.com>
2025-01-09 11:16:39 +0100

    using one object

--- file.js
+++ file.js
@@ -1,262 +1,63 @@
-const Erngs = [
-    {
-        img_src : 'boucle1.jpg',
-        item_name : "Fleur boucles d\'oreilles",
-        price : 260,
-        qte_id : 'bcl1',
-        icon_id : 'icon_bcl1',
-    },
-    {
-        img_src : 'boucle2.jpg',
-        item_name : 'Glowy boucles d\'orielles',
-        price : 390,
-        qte_id : 'bcl2',
-        icon_id : 'icon_bcl2',
-    },
-    {
-        img_src : 'boucle3.jpg',
-        item_name : 'Ajourée main de fatma boucles d\'oreilles',
-        price : 140,
-        qte_id : 'bcl3',
-        icon_id : 'icon_bcl3',
-    },
-    {
-        img_src : 'boucle4.jpg',
-        item_name : 'Round boucles d\'oreilles',
-        price : 570,
-        qte_id : 'bcl4',
-        icon_id : 'icon_bcl4',
-    },
-    {
-        img_src : 'boucle5.jpg',
-        item_name : 'Little rose boucles d\'oreilles',
-        price : 200,
-        qte_id : 'bcl5',
-        icon_id : 'icon_bcl5',
-    },
-    {
-        img_src : 'boucle6.jpg',
-        item_name : 'Purity boucles d\'oreilles',
-        price : 190,
-        qte_id : 'bcl6',
-        icon_id : 'icon_bcl6',
-    },
-]
-
-var divEarings = document.getElementById('Earings')
-divEarings.setAttribute('class', 'flex flex-wrap justify-center gap-14')
-for(let i = 0; i < Erngs.length; i++){
-    let safeItemName = Erngs[i]['item_name'].replace(/'/g, "\\'");
-    var div_cart = document.createElement('div')
-    div_cart.setAttribute('class', 'col relative  hover:shadow-xl  w-1/4 text-[#212529] border-[0.3px] bg-gray-50  rounded-md overflow-hidden')
-    div_cart.innerHTML = `
-        <img src="images/${Erngs[i]['img_src']}" class="card-img-top w-full h-80 border-b " alt="">
-        <div onclick="addToFvrt('images/${Erngs[i]['img_src']}', '${safeItemName}', ${Erngs[i]['price']}, '${Erngs[i]['icon_id']}')" class="flex justify-center items-center absolute top-3 right-3 bg-gray-100 hover:bg-pink-200 cursor-pointer w-10 h-10 rounded-full">
-            <i id="${Erngs[i]['icon_id']}" class=" fa-regular fa-heart fa-xl" style="color: #e6447a;"></i>
-        </div>
-        <div class="card_body flex flex-col items-center pt-3">
-            <h5 class="card-title h-14  text-lg font-semibold text-center leading-6 text-[#212529] mb-2 px-2">${Erngs[i]['item_name']}</h5>
-            <h5 class="card-title  text-lg font-semibold text-center leading-6 text-[#212529] mb-2">${Erngs[i]['price']} dh</h5>
-            <div class="w-full h-9 py-5 px-16 bg-[#e6447a] text-white text-base flex justify-between items-center mt-2">
-                <button onclick="minus('images/${Erngs[i]['img_src']}', '${safeItemName}', ${Erngs[i]['price']}, '${Erngs[i]['qte_id']}')" class="text-white font-semibold text-xl">-</button>
-                <p id="${Erngs[i]['qte_id']}" class="text-white font-semibold text-xl">0</p>
-                <button type="submit" onclick="plus('images/${Erngs[i]['img_src']}', '${safeItemName}', ${Erngs[i]['price']}, '${Erngs[i]['qte_id']}')" class="text-white font-semibold text-xl">+</button>
-            </div>
-        </div>
-    `
-        
-    divEarings.appendChild(div_cart)
+const sections = {
+    Earings : [
+        {img_src : 'boucle1.jpg', item_name : "Fleur boucles d\'oreilles", price : 260, qte_id : 'bcl1', icon_id : 'icon_bcl1',},
+        {img_src : 'boucle2.jpg', item_name : 'Glowy boucles d\'orielles', price : 390, qte_id : 'bcl2', icon_id : 'icon_bcl2',},
+        {img_src : 'boucle3.jpg', item_name : 'Ajourée main de fatma boucles d\'oreilles', price : 140, qte_id : 'bcl3', icon_id : 'icon_bcl3',}, 
+        {img_src : 'boucle4.jpg', item_name : 'Round boucles d\'oreilles', price : 570, qte_id : 'bcl4', icon_id : 'icon_bcl4',},
+        {img_src : 'boucle5.jpg', item_name : 'Little rose boucles d\'oreilles', price : 200, qte_id : 'bcl5', icon_id : 'icon_bcl5',},
+        {img_src : 'boucle6.jpg', item_name : 'Purity boucles d\'oreilles', price : 190, qte_id : 'bcl6', icon_id : 'icon_bcl6',},
+    ],
+    Rings : [
+        {img_src : 'baguef1.jpg', item_name : 'Bague ouverte Idyllia', price : 129, qte_id : 'bg1', icon_id : 'icon_bg1',},
+        {img_src : 'baguef2.jpg', item_name : 'Bague cocktail Hyperbola', price : 220, qte_id : 'bg2', icon_id : 'icon_bg2',},
+        {img_src : 'baguef3.jpg', item_name : 'Bague Idyllia', price : 200, qte_id : 'bg3', icon_id : 'icon_bg3',},
+    ],
+    Necklaces : [
+        {img_src : 'collier1.jpg', item_name : 'Prestige collier', price : 260, qte_id : 'col1', icon_id : 'icon_col1',},
+        {img_src : 'collier2.jpg', item_name : 'Eye collier', price : 220, qte_id : 'col2', icon_id : 'icon_col2',},
+        {img_src : 'collier3.jpg', item_name : 'Little lily collier', price : 245, qte_id : 'col3', icon_id : 'icon_col3',},
+        {img_src : 'collier4.jpg', item_name : 'Elegent collier', price : 225,  qte_id : 'col4',  icon_id : 'icon_col4',},
+        {img_src : 'collier5.jpg', item_name : 'Zelij Tiny Collier', price : 300, qte_id : 'col5', icon_id : 'icon_col5',},
+        {img_src : 'collier6.jpg', item_name : 'Beauty collier', price : 370, qte_id : 'col6', icon_id : 'icon_col6',},
+    ],
+    Watches : [
+        {img_src : 'montref1.jpg', item_name : 'Montre Octea Chrono', price : 430, qte_id : 'mtr1', icon_id : 'icon_mtr1',},
+        {img_src : 'montref2.jpg', item_name : 'Montre Certa', price : 350, qte_id : 'mtr2', icon_id : 'icon_mtr2',},
+        {img_src : 'montref3.png', item_name : 'Montre Octea Lux Sport', price : 430, qte_id : 'mtr3', icon_id : 'icon_mtr3',},
+        {img_src : 'montref4.jpg', item_name : 'Montre Cosmopolitan', price : 250, qte_id : 'mtr4', icon_id : 'icon_mtr4',},
+        {img_src : 'montref5.jpg', item_name : 'Montre Crystalline Aura', price : 430, qte_id : 'mtr5', icon_id : 'icon_mtr5',},
+        {img_src : 'montref6.jpg', item_name : 'Montre Crystalline Delight', price : 350, qte_id : 'mtr6', icon_id : 'icon_mtr6',},
+    ]
 }
 
-const Rings = [
-    {
-        img_src : 'baguef1.jpg',
-        item_name : 'Bague ouverte Idyllia',
-        price : 129,
-        qte_id : 'bg1',
-        icon_id : 'icon_bg1',
-    },
-    {
-        img_src : 'baguef2.jpg',
-        item_name : 'Bague cocktail Hyperbola',
-        price : 220,
-        qte_id : 'bg2',
-        icon_id : 'icon_bg2',
-    },
-    {
-        img_src : 'baguef3.jpg',
-        item_name : 'Bague Idyllia',
-        price : 200,
-        qte_id : 'bg3',
-        icon_id : 'icon_bg3',
-    },
-]
-
-var divRings = document.getElementById('Rings')
-divRings.setAttribute('class', 'flex flex-wrap justify-center gap-14')
-for(let i = 0; i < Rings.length; i++){
-    let safeItemName = Rings[i]['item_name'].replace(/'/g, "\\'");
-    var div_cart = document.createElement('div')
-    div_cart.setAttribute('class', 'col relative  hover:shadow-xl  w-1/4 text-[#212529] border-[0.3px] bg-gray-50  rounded-md overflow-hidden')
-    div_cart.innerHTML = `
-        <img src="images/${Rings[i]['img_src']}" class="card-img-top w-full h-80 border-b " alt="">
-        <div onclick="addToFvrt('images/${Rings[i]['img_src']}', '${safeItemName}', ${Rings[i]['price']}, '${Rings[i]['icon_id']}')" class="flex justify-center items-center absolute top-3 right-3 bg-gray-100 hover:bg-pink-200 cursor-pointer w-10 h-10 rounded-full">
-            <i id="${Rings[i]['icon_id']}" class=" fa-regular fa-heart fa-xl" style="color: #e6447a;"></i>
-        </div>
-        <div class="card_body flex flex-col items-center pt-3">
-            <h5 class="card-title h-14  text-lg font-semibold text-center leading-6 text-[#212529] mb-2 px-2">${Rings[i]['item_name']}</h5>
-            <h5 class="card-title  text-lg font-semibold text-center leading-6 text-[#212529] mb-2">${Rings[i]['price']} dh</h5>
-            <div class="w-full h-9 py-5 px-16 bg-[#e6447a] text-white text-base flex justify-between items-center mt-2">
-                <button onclick="minus('images/${Rings[i]['img_src']}', '${safeItemName}', ${Rings[i]['price']}, '${Rings[i]['qte_id']}')" class="text-white font-semibold text-xl">-</button>
-                <p id="${Rings[i]['qte_id']}" class="text-white font-semibold text-xl">0</p>
-                <button type="submit" onclick="plus('images/${Rings[i]['img_src']}', '${safeItemName}', ${Rings[i]['price']}, '${Rings[i]['qte_id']}')" class="text-white font-semibold text-xl">+</button>
-            </div>
-        </div>
-    `
-        
-    divRings.appendChild(div_cart)
-}
-
-const Ncklcs = [
-    {
-        img_src : 'collier1.jpg',
-        item_name : 'Prestige collier',
-        price : 260,
-        qte_id : 'col1',
-        icon_id : 'icon_col1',
-    },
-    {
-        img_src : 'collier2.jpg',
-        item_name : 'Eye collier',
-        price : 220,
-        qte_id : 'col2',
-        icon_id : 'icon_col2',
-    },
-    {
-        img_src : 'collier3.jpg',
-        item_name : 'Little lily collier',
-        price : 245,
-        qte_id : 'col3',
-        icon_id : 'icon_col3',
-    },
-    {
-        img_src : 'collier4.jpg',
-        item_name : 'Elegent collier',
-        price : 225,
-        qte_id : 'col4',
-        icon_id : 'icon_col4',
-    },
-    {
-        img_src : 'collier5.jpg',
-        item_name : 'Zelij Tiny Collier',
-        price : 300,
-        qte_id : 'col5',
-        icon_id : 'icon_col5',
-    },
-    {
-        img_src : 'collier6.jpg',
-        item_name : 'Beauty collier',
-        price : 370,
-        qte_id : 'col6',
-        icon_id : 'icon_col6',
-    },
-]
-
-var divNcklcs = document.getElementById('Necklaces')
-divNcklcs.setAttribute('class', 'flex flex-wrap justify-center gap-14')
-for(let i = 0; i < Ncklcs.length; i++){
-    let safeItemName = Ncklcs[i]['item_name'].replace(/'/g, "\\'");
-    var div_cart = document.createElement('div')
-    div_cart.setAttribute('class', 'col relative  hover:shadow-xl  w-1/4 text-[#212529] border-[0.3px] bg-gray-50  rounded-md overflow-hidden')
-    div_cart.innerHTML = `
-        <img src="images/${Ncklcs[i]['img_src']}" class="card-img-top w-full h-80 border-b " alt="">
-        <div onclick="addToFvrt('images/${Ncklcs[i]['img_src']}', '${safeItemName}', ${Ncklcs[i]['price']}, '${Ncklcs[i]['icon_id']}')" class="flex justify-center items-center absolute top-3 right-3 bg-gray-100 hover:bg-pink-200 cursor-pointer w-10 h-10 rounded-full">
-            <i id="${Ncklcs[i]['icon_id']}" class=" fa-regular fa-heart fa-xl" style="color: #e6447a;"></i>
-        </div>
-        <div class="card_body flex flex-col items-center pt-3">
-            <h5 class="card-title h-14  text-lg font-semibold text-center leading-6 text-[#212529] mb-2 px-2">${Ncklcs[i]['item_name']}</h5>
-            <h5 class="card-title  text-lg font-semibold text-center leading-6 text-[#212529] mb-2">${Ncklcs[i]['price']} dh</h5>
-            <div class="w-full h-9 py-5 px-16 bg-[#e6447a] text-white text-base flex justify-between items-center mt-2">
-                <button onclick="minus('images/${Ncklcs[i]['img_src']}', '${safeItemName}', ${Ncklcs[i]['price']}, '${Ncklcs[i]['qte_id']}')" class="text-white font-semibold text-xl">-</button>
-                <p id="${Ncklcs[i]['qte_id']}" class="text-white font-semibold text-xl">0</p>
-                <button type="submit" onclick="plus('images/${Ncklcs[i]['img_src']}', '${safeItemName}', ${Ncklcs[i]['price']}, '${Ncklcs[i]['qte_id']}')" class="text-white font-semibold text-xl">+</button>
-            </div>
-        </div>
-    `
-        
-    divNcklcs.appendChild(div_cart)
-}
-
-
-const Wtchs = [
-    {
-        img_src : 'montref1.jpg',
-        item_name : 'Montre Octea Chrono',
-        price : 430,
-        qte_id : 'mtr1',
-        icon_id : 'icon_mtr1',
-    },
-    {
-        img_src : 'montref2.jpg',
-        item_name : 'Montre Certa',
-        price : 350,
-        qte_id : 'mtr2',
-        icon_id : 'icon_mtr2',
-    },
-    {
-        img_src : 'montref3.png',
-        item_name : 'Montre Octea Lux Sport',
-        price : 430,
-        qte_id : 'mtr3',
-        icon_id : 'icon_mtr3',
-    },
-    {
-        img_src : 'montref4.jpg',
-        item_name : 'Montre Cosmopolitan',
-        price : 250,
-        qte_id : 'mtr4',
-        icon_id : 'icon_mtr4',
-    },
-    {
-        img_src : 'montref5.jpg',
-        item_name : 'Montre Crystalline Aura',
-        price : 430,
-        qte_id : 'mtr5',
-        icon_id : 'icon_mtr5',
-    },
-    {
-        img_src : 'montref6.jpg',
-        item_name : 'Montre Crystalline Delight',
-        price : 350,
-        qte_id : 'mtr6',
-        icon_id : 'icon_mtr6',
-    },
-]
-
-var divWtchs = document.getElementById('Watches')
-divWtchs.setAttribute('class', 'flex flex-wrap justify-center gap-14')
-for(let i = 0; i < Wtchs.length; i++){
-    let safeItemName = Wtchs[i]['item_name'].replace(/'/g, "\\'");
-    var div_cart = document.createElement('div')
-    div_cart.setAttribute('class', 'col relative  hover:shadow-xl  w-1/4 text-[#212529] border-[0.3px] bg-gray-50  rounded-md overflow-hidden')
-    div_cart.innerHTML = `
-        <img src="images/${Wtchs[i]['img_src']}" class="card-img-top w-full h-80 border-b " alt="">
-        <div onclick="addToFvrt('images/${Wtchs[i]['img_src']}', '${safeItemName}', ${Wtchs[i]['price']}, '${Wtchs[i]['icon_id']}')" class="flex justify-center items-center absolute top-3 right-3 bg-gray-100 hover:bg-pink-200 cursor-pointer w-10 h-10 rounded-full">
-            <i id="${Wtchs[i]['icon_id']}" class=" fa-regular fa-heart fa-xl" style="color: #e6447a;"></i>
-        </div>
-        <div class="card_body flex flex-col items-center pt-3">
-            <h5 class="card-title h-14  text-lg font-semibold text-center leading-6 text-[#212529] mb-2 px-2">${Wtchs[i]['item_name']}</h5>
-            <h5 class="card-title  text-lg font-semibold text-center leading-6 text-[#212529] mb-2">${Wtchs[i]['price']} dh</h5>
-            <div class="w-full h-9 py-5 px-16 bg-[#e6447a] text-white text-base flex justify-between items-center mt-2">
-                <button onclick="minus('images/${Wtchs[i]['img_src']}', '${safeItemName}', ${Wtchs[i]['price']}, '${Wtchs[i]['qte_id']}')" class="text-white font-semibold text-xl">-</button>
-                <p id="${Wtchs[i]['qte_id']}" class="text-white font-semibold text-xl">0</p>
-                <button type="submit" onclick="plus('images/${Wtchs[i]['img_src']}', '${safeItemName}', ${Wtchs[i]['price']}, '${Wtchs[i]['qte_id']}')" class="text-white font-semibold text-xl">+</button>
-            </div>
-        </div>
-    `
-        
-    divWtchs.appendChild(div_cart)
-}
+    for(var section in sections){
+        var divSection = document.getElementById(section)
+        divSection.setAttribute('class', 'flex flex-wrap  gap-14')
+        for(var x of sections[section]){
+            console.log("x: ", x['item_name'])
+            
+                let safeItemName = x['item_name'].replace(/'/g, "\\'");
+                var div_cart = document.createElement('div')
+                div_cart.setAttribute('class', 'col relative  hover:shadow-xl  w-1/4 m-auto text-[#212529] border-[0.3px] bg-gray-50  rounded-md overflow-hidden')
+                div_cart.innerHTML = `
+                    <img src="images/${x['img_src']}" class="card-img-top w-full h-80 border-b " alt="">
+                    <div onclick="addToFvrt('images/${x['img_src']}', '${safeItemName}', ${x['price']}, '${x['icon_id']}')" class="flex justify-center items-center absolute top-3 right-3 bg-gray-100 hover:bg-pink-200 cursor-pointer w-10 h-10 rounded-full">
+                        <i id="${x['icon_id']}" class=" fa-regular fa-heart fa-xl" style="color: #e6447a;"></i>
+                    </div>
+                    <div class="card_body flex flex-col items-center pt-3">
+                        <h5 class="card-title h-14  text-lg font-semibold text-center leading-6 text-[#212529] mb-2 px-2">${x['item_name']}</h5>
+                        <h5 class="card-title  text-lg font-semibold text-center leading-6 text-[#212529] mb-2">${x['price']} dh</h5>
+                        <div class="w-full h-9 py-5 px-16 bg-[#e6447a] text-white text-base flex justify-between items-center mt-2">
+                            <button onclick="minus('images/${x['img_src']}', '${safeItemName}', ${x['price']}, '${x['qte_id']}')" class="text-white font-semibold text-xl">-</button>
+                            <p id="${x['qte_id']}" class="text-white font-semibold text-xl">0</p>
+                            <button type="submit" onclick="plus('images/${x['img_src']}', '${safeItemName}', ${x['price']}, '${x['qte_id']}')" class="text-white font-semibold text-xl">+</button>
+                        </div>
+                    </div>
+                `
+                divSection.appendChild(div_cart)
+        }
+    }
+    
 
     function navigateToSection(id) {
         if (id !== "#") {
